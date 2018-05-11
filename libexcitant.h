9fc88ada567c6ac74363e0e1e93fe561fc1612b1
chilaxan <35645806+chilaxan@users.noreply.github.com>
2018-05-11 14:08:45 -0400

    Convert to use libexcitant

---
+++ libexcitant.h
@@ -0,0 +1,17 @@
+@interface Excitant : NSObject
++(void)AUXtoggleFlash;
++(void)AUXtoggleLPM;
++(void)AUXtoggleAirplaneMode;
++(void)AUXtoggleMute;
++(void)AUXtoggleRotationLock;
++(void)AUXcontrolCenter;
++(void)AUXrespring;
++(void)AUXlaunchApp:(id)arg1;
++(void)AUXlockDevice;
++(void)AUXhomePress;
++(BOOL)AUXisDeviceLocked;
++(void)AUXtoggleBluetooth;
++(BOOL)AUXisDeviceOnSB;
++(BOOL)AUXisDeviceOnNC;
++(void)AUXtoggleWiFi;
+@end
